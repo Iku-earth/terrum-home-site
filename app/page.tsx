7335d01df3f6cf3d994fb515397128edd7b80f13
Abhishek AN <me@abhishekan.dev>
2023-07-25 23:11:35 +0530

    Add initial site

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,35 @@
-import Image from 'next/image'
+import { LandingHeader } from "@/components/home-page-header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col min-h-screen">
+      <LandingHeader />
+      <div className="w-full h-max flex flex-grow justify-center items-center overflow-hidden text-center backdrop-blur-3xl px-4">
+        <div>
+          <h1 className="text-2xl md:text-3xl lg:text-5xl text-slate-200 font-extrabold">
+            One Earth, One Community<br></br>
+            <p className="h-fit text-3xl font-sans md:text-4xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#0095D8] to-[#6D8DCB] py-2">
+              Taking Action for Climate Change
+            </p>
+          </h1>
+          <p className="lg:w-3/5 mx-auto text-base lg:text-xl text-slate-200 px-2 md:px-16 py-4">
+            Terrum is a community platform uniting climate activists and
+            change-makers in a gamified journey to combat the climate crisis.
+            Share your initiatives, inspire others, and take meaningful climate
+            action together.
+          </p>
+          <div className="w-full h-fit flex flex-col place-items-center my-8 lg:my-12 z-50 focus:outline-none focus:bg-transparent select-none">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.iku"
+              target="_blank"
+            >
+              <p className="w-fit px-12 py-4 rounded-full hover:no-underline text-slate-900 lg:hover:text-slate-200 text-lg font-semibold bg-slate-200 shadow-md lg:hover:shadow-[#0067A9] lg:hover:bg-slate-200 lg:hover:bg-opacity-10 lg::hover:text-[#0067A9] z-[1000] transition-all duration-500 lg:hover:translate-y-[-10px] select-none">
+                Download Terrum &nbsp; 🌏
+              </p>
+            </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
---
+++ components/home-page-header.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export const LandingHeader = () => {
+  const [isScrolled, setIsScrolled] = useState(() => window.scrollY > 0);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  useEffect(() => {
+    // Add scroll event listener
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <header
+      className={`sticky top-0 z-50 p-4 ${
+        isScrolled ? "backdrop-blur" : "bg-transparent"
+      }`}
+    >
+      {/* Add an additional wrapper for the blurred background */}
+      {isScrolled && <div className="backdrop-blur-md absolute inset-0"></div>}
+      <div className="relative flex items-center justify-between w-full">
+        <div className="flex items-center">
+          {/* Replace 'logo.svg' with the path to your logo image */}
+          <img
+            src="terrum_circle.svg"
+            alt="Logo"
+            className="w-12 h-auto lg:ml-8"
+          />
+          <h1 className={`text-lg font-semibold ml-5`}>Terrum</h1>
+        </div>
+
+        {/* Show hamburger menu in mobile view */}
+        {/* <div className="lg:hidden">
+          <button
+            onClick={toggleMobileMenu}
+            className="p-2 text-white focus:outline-none"
+          >
+            <svg
+              className={`w-6 h-6 ${isMobileMenuOpen ? "hidden" : "block"}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+            <svg
+              className={`w-6 h-6 ${isMobileMenuOpen ? "block" : "hidden"}`}
+              fill="none"
+              stroke="white"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
+        </div> */}
+
+        {/* Regular navigation links */}
+        <div className="hidden lg:flex items-center">
+          {/* Add items-center to center the Support option */}
+          {/* <div className="mr-4">Support</div> */}
+          {/* Add the login button */}
+          {/* <button
+            onClick={() => {}}
+            className="px-4 py-2 rounded-md bg-slate-200 text-black mr-5"
+          >Login</button> */}
+        </div>
+      </div>
+      {/* Mobile menu */}
+      <div
+        className={`lg:hidden w-full bg-black z-40 p-4 absolute top-20 right-0 transform transition-transform duration-300 ${
+          isMobileMenuOpen ? "scale-y-100" : "scale-y-0"
+        }`}
+        style={{ transformOrigin: "top" }}
+      >
+        <div className="flex flex-col items-end space-y-4">
+          {/* Replace with mobile navigation links */}
+          <div className="text-white">Support</div>
+          <div className="text-white">Login</div>
+        </div>
+      </div>
+    </header>
+  );
+};
