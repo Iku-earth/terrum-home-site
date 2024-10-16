db03ec570ea1aa80a95fd66816f0450c1a596934
Abhishek AN <me@abhishekan.dev>
2024-10-16 22:45:29 +0530

    Version 2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { LandingHeader } from "@/components/home-page-header";
 import "./globals.css";
 import { Inter, Nunito } from "next/font/google";
-import Link from "next/link";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 const nunito = Nunito({
@@ -23,32 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${nunito.variable}`}>
-        <div className="flex flex-col min-h-screen bg-black">
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#071D2B] to-[#111111]">
           <LandingHeader />
           {children}
-          <footer className="w-full grid grid-flow-col gap-5 justify-center mb-5 text-gray-400">
-            <Link
-              href="https://discord.gg/c4869U2JjK"
-              className="group transition duration-300"
-            >
-              Discord
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-blue-800"></span>
-            </Link>
-            <Link
-              href="https://www.instagram.com/terrum.in/"
-              className="group transition duration-300"
-            >
-              Instagram
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-blue-800"></span>
-            </Link>
-            <Link
-              href="/privacy/policy"
-              className="group transition duration-300"
-            >
-              Privacy Policy
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-blue-800"></span>
-            </Link>
-          </footer>
+          <Footer />
         </div>
       </body>
     </html>
