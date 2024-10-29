9c4be6d81e5c450488950fa5e48a5df98edc48ff
Abhishek AN <me@abhishekan.dev>
2024-10-30 01:53:00 +0530

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,53 @@
 import EventGrid from "@/components/landing/events-and-experiences/EventGrid";
 import VolunteerWithUsSection from "@/components/volunteer-with-us";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terrum",
+  description:
+    "Terrum is a community-driven platform empowering individuals to take meaningful climate action through engaging events, volunteer opportunities, and inspiring stories. Join us on a journey toward a sustainable future.",
+  keywords: [
+    "Climate action",
+    "Sustainability",
+    "Community",
+    "Environmental activism",
+    "Volunteering",
+    "Nature walks",
+    "Eco-friendly initiatives",
+    "Abhishek AN",
+    "Renuka Pooja",
+  ],
+  icons: {
+    icon: "/terrum_circle_64x64.ico",
+    apple: "/terrum_circle_64x64.png",
+  },
+  openGraph: {
+    title: "Join Terrum – One Earth, One Community",
+    locale: "en_US",
+    description:
+      "Connect with change-makers and activists at Terrum to combat climate change together. Share your journey, attend events, and inspire action.",
+    url: "https://terrum.in",
+    type: "website",
+    images: [
+      {
+        url: "https://terrum.in/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Terrum Banner",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@terrum_in",
+    title: "Terrum – Join the Movement for Climate Action",
+    description:
+      "Connect with passionate individuals for impactful climate action. Terrum offers volunteer opportunities, community events, and resources for sustainable living.",
+    images: [
+      "https://pbs.twimg.com/profile_images/1776445625254793216/19fhNGuT_400x400.jpg",
+    ],
+  },
+};
 
 export default function Home() {
   return (
