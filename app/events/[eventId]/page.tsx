2ee74d1a54d0505c31e27daf3e2af74f32cf69de
Abhishek AN <me@abhishekan.dev>
2025-02-27 15:26:02 +0530

    Dynamic event page

---
+++ app/events/[eventId]/page.tsx
@@ -0,0 +1,69 @@
+import { notFound } from "next/navigation"
+
+interface Event {
+  name: string
+  description: string
+  image: {
+    alt: string
+    presigned_url: string
+  }
+  venue: string | null
+  city: string | null
+  locality: string | null
+  google_maps_link: string | null
+  is_single_day: boolean
+  start_date: string
+  end_date: string | null
+  start_time: string | null
+  end_time: string | null
+  deleted: boolean
+  is_online: boolean
+  location: Record<string, any>
+  google_form_link: string | null
+  payment_link: string | null
+  event_uuid: string
+}
+
+// Fetch event data from the API
+async function getEvent(eventId: string): Promise<Event> {
+  
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/cms/events/?event_uuid=${eventId}`
+  );
+
+  if (!res.ok) {
+    console.error("Failed to fetch event:", res.statusText);
+  }
+
+  return res.json()
+}
+
+export default async function EventPage({ params }: { params: { eventId: string } }) {
+  // Get the event ID from the URL
+  const { eventId } = await params
+
+  // Fetch the event data
+  let event: Event
+
+  try {
+    event = await getEvent(eventId)
+  } catch (error) {
+    console.error("Error fetching event:", error)
+    notFound()
+  }
+
+  return (
+    <div className="bg-gradient-to-b from-[#071D2B] to-[#111111]">
+      <div className="flex justify-center items-center h-screen w-screen">
+        <iframe
+          src={event.google_form_link + "?embedded=true"}
+          className="w-full h-full border-0"
+          title="Google Form"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
+}
+
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains:
+      process.env.NODE_ENV === "production"
+        ? ["terrum-prod.s3.amazonaws.com"]
+        : ["terrum-development.s3.amazonaws.com"],
+  },
+};
 
 module.exports = nextConfig
